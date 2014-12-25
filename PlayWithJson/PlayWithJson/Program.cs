5d2ee7fea5f718c9f6e226bcede8909cca63bd9a
sikokimo <i.miss.uu@hotmail.com>
2014-12-25 03:21:44 +0200

    First GitHub commit

---
+++ PlayWithJson/PlayWithJson/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayWithJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Json To GitHub");
+        }
+    }
+}
